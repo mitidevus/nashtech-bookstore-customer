40e5ab40e7e45f70b80bb4cbc09341905e4b8285
Do Minh Tri <mitidevus@gmail.com>
2024-05-31 00:52:08 +0700

    feat: get books by category slug

--- src/store/api/category/categoryApiSlice.ts
+++ src/store/api/category/categoryApiSlice.ts
@@ -1,3 +1,6 @@
+import { SortBy } from "constants/sort";
+import { GetListResult } from "types/app";
+import { Book } from "types/book";
 import { Category } from "types/category";
 import { apiSlice } from "../baseApiSlice";
 
@@ -7,8 +10,30 @@ const categoryApiSlice = apiSlice.injectEndpoints({
       query: () => "categories",
       transformResponse: (response: { data: Category[] }) => response.data,
     }),
+    getBooksByCategorySlug: build.query<
+      Category & { books: GetListResult<Book> },
+      {
+        slug: string;
+        page: number;
+        take: number;
+        sort?: SortBy;
+      }
+    >({
+      query: ({ slug, page, take, sort }) => ({
+        url: `categories/${slug}/books`,
+        params: {
+          page,
+          take,
+          sort,
+        },
+      }),
+    }),
   }),
 });
 
-export const { useGetCategoriesQuery, useLazyGetCategoriesQuery } =
-  categoryApiSlice;
+export const {
+  useGetCategoriesQuery,
+  useLazyGetCategoriesQuery,
+  useGetBooksByCategorySlugQuery,
+  useLazyGetBooksByCategorySlugQuery,
+} = categoryApiSlice;
