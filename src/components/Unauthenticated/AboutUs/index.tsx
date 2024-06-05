5d838f5179090f811d456464603cfa11c11cd653
Do Minh Tri <mitidevus@gmail.com>
2024-06-05 17:10:51 +0700

    feat: about page

---
+++ src/components/Unauthenticated/AboutUs/index.tsx
@@ -0,0 +1,36 @@
+import { Box, Paper } from "@mui/material";
+import { Breadcrumbs } from "components/Common/Breadcrumbs";
+import CenterLoading from "components/Common/CenterLoading";
+import NoData from "components/Common/NoData";
+import { useGetAboutQuery } from "store/api/about/aboutApiSlice";
+
+export default function AboutUs() {
+  const { data, isLoading } = useGetAboutQuery();
+
+  if (isLoading) {
+    return <CenterLoading />;
+  }
+
+  return (
+    <>
+      <Breadcrumbs />
+
+      <Paper sx={{ p: 2 }}>
+        {data ? (
+          <Box dangerouslySetInnerHTML={{ __html: data }} />
+        ) : (
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "50vh",
+            }}
+          >
+            <NoData message="Something went wrong! Cannot get about us content" />
+          </Box>
+        )}
+      </Paper>
+    </>
+  );
+}
